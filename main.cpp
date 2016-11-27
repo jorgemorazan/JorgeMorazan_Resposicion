70bdacd49995e7ddb3d96f93fbe6767dea7481ec
Jorge Morazan <jorgemorazan@unitec.edu>
2016-11-26 18:45:24 -0600

    13

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int main(){
 			int sure=0;
 			int numA=0,denA=0,numB=0,denB=0;
 			do{
-				U
 				cout<<"Ingrese su numerador de la primera racional: ";
 				cin>>numA;
 				cout<<"Ingrese su denominador de la primera racional: ";
